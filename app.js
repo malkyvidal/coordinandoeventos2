c523d578eb4f75f62c3af7d873566a247a24e772
malky vidal <malkyvidal@gmail.com>
2019-11-01 15:05:55 -0300

    app

--- app.js
+++ app.js
@@ -38,12 +38,11 @@ mouseDown$.pipe(
 
 
  
-let isMouseDown = false;
+/* let isMouseDown = false;
 let itemsContainer = document.getElementById("items-container");
-mouseOffset = {x:0,y:0};
+let mouseOffset = {x:0,y:0};
 let todosContainer = document.getElementById("todos-container");
 let item = document.getElementById("todo-item");
-
 item.addEventListener("mousedown", (e) => { onMouseDown(e, item); });
     
     document.body.addEventListener("mousemove", (e) => {
@@ -56,25 +55,18 @@ item.addEventListener("mousedown", (e) => { onMouseDown(e, item); });
 
 function onMouseDown(e, item) {  
   isMouseDown = true;
-
   mouseOffset = {x: item.offsetLeft - e.clientX, y: item.offsetTop - e.clientY};
-
   item.style.backgroundColor = "#6edc9c";
 }
-
 function onMouseUp(e, item) {
-    
   isMouseDown = false;
   item.style.backgroundColor = "#06883c";
 }
-
 function onMouseMove(e, item) {
     console.log('mouse moviendose')
     e.preventDefault(); 
     if(isMouseDown) {
-     
       item.style.left = e.clientX + mouseOffset.x + "px";
       item.style.top = e.clientY + mouseOffset.y + "px";
-    
     }
-  }  
+  }   */
