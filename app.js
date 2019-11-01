03b2beb3ac481feef223591a29c9e935e330b856
malky vidal <malkyvidal@gmail.com>
2019-11-01 14:15:01 -0300

    cambios js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/* 
 const { fromEvent } = require('rxjs');
 const { map, takeUntil, switchMap,tap } = require('rxjs/operators');
 
@@ -6,7 +6,7 @@ let draggable = document.getElementById('todo-item');
 let mouseDown$ = fromEvent(draggable, 'mousedown');
 let mouseMove$ = fromEvent(document, 'mousemove')
 let mouseUp$ = fromEvent(document, 'mouseup').pipe(
-    tap(()=>draggable.style.backgroundColor="#F44336"),
+    tap(()=>draggable.style.backgroundColor="#06883c"),
 );
 
 let movimiento$ = mouseMove$
@@ -24,7 +24,7 @@ let movimiento$ = mouseMove$
     );
 
 mouseDown$.pipe(
-    tap(()=>draggable.style.backgroundColor="#E57373"),
+    tap(()=>draggable.style.backgroundColor="#6edc9c"),
     switchMap(() => movimiento$)
 ).subscribe(pos => {
     // Draggable is absolutely positioned
@@ -32,23 +32,19 @@ mouseDown$.pipe(
     draggable.style.left = pos.x + 'px';
     draggable.style.top = pos.y + 'px';
 });
+ */
 
 
 
 
-
-/* 
- let isMouseDown = false;
+ 
+let isMouseDown = false;
 let itemsContainer = document.getElementById("items-container");
 mouseOffset = {x:0,y:0};
-
 let todosContainer = document.getElementById("todos-container");
-
 let item = document.getElementById("todo-item");
 
-
-    
-    item.addEventListener("mousedown", (e) => { onMouseDown(e, item); });
+item.addEventListener("mousedown", (e) => { onMouseDown(e, item); });
     
     document.body.addEventListener("mousemove", (e) => {
         onMouseMove(e, item);
@@ -58,21 +54,18 @@ let item = document.getElementById("todo-item");
       onMouseUp(e, item);
     });
 
-
-  
-function onMouseDown(e, item) {
-    
+function onMouseDown(e, item) {  
   isMouseDown = true;
 
   mouseOffset = {x: item.offsetLeft - e.clientX, y: item.offsetTop - e.clientY};
 
-  item.style.backgroundColor = "#E57373";
+  item.style.backgroundColor = "#6edc9c";
 }
 
 function onMouseUp(e, item) {
     
   isMouseDown = false;
-  item.style.backgroundColor = "#F44336";
+  item.style.backgroundColor = "#06883c";
 }
 
 function onMouseMove(e, item) {
@@ -84,4 +77,4 @@ function onMouseMove(e, item) {
       item.style.top = e.clientY + mouseOffset.y + "px";
     
     }
-  }  */
+  }  
